b8775b7b33af3d0814c0609359b594a025c992ca
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-12-21 20:28:42 +0800

    Add files via upload

---
+++ leetcode_no746_使用最小花费爬楼梯_w1.cpp
@@ -0,0 +1,73 @@
+﻿/* ------------------------------------------------------------------|
+数组的每个索引作为一个阶梯，第 i个阶梯对应着一个非负数的体力花费值 cost[i](索引从0开始)。
+
+每当你爬上一个阶梯你都要花费对应的体力花费值，然后你可以选择继续爬一个阶梯或者爬两个阶梯。
+
+您需要找到达到楼层顶部的最低花费。在开始时，你可以选择从索引为 0 或 1 的元素作为初始阶梯。
+
+示例 1:
+
+输入: cost = [10, 15, 20]
+输出: 15
+解释: 最低花费是从cost[1]开始，然后走两步即可到阶梯顶，一共花费15。
+ 示例 2:
+
+输入: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+输出: 6
+解释: 最低花费方式是从cost[0]开始，逐个经过那些1，跳过cost[3]，一共花费6。
+注意：
+
+cost 的长度将会在 [2, 1000]。
+每一个 cost[i] 将会是一个Integer类型，范围为 [0, 999]。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/min-cost-climbing-stairs
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：8 ms, 在所有 C++ 提交中击败了92.51%的用户
+*	内存消耗：13.6 MB, 在所有 C++ 提交中击败了25.52%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int minCostClimbingStairs(vector<int>& cost) {
+    int n = cost.size();
+    int curr = 0;
+    int pre = 0;
+    for (int i = 2; i <= n; i++) {
+        int next = min(curr + cost[i - 1], pre + cost[i - 2]);
+        pre = curr;
+        curr = next;
+    }
+    return curr;
+}
+
+int main() {
+    vector<int> cost = { 0,0,1,1 };
+    int res = minCostClimbingStairs(cost);
+}
